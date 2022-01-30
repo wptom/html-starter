3139c5573c9660c566242daaed3dac0e4a6b5c1c
wptom.com <hello@wptom.com>
2022-01-30 17:43:43 +0100

    Minor improvements.

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const express = require('express');
 const path = require('path');
-
-const app = express();
 const ejs = require('ejs');
 
-app.set('view engine','ejs');
+const app = express();
 
 app.use(express.static('public'));
+
+app.set('view engine','ejs');
 
 app.listen(3000, () => {
     console.log('App listening on port 3000');
 });
 
 app.get('/',(req,res) => {
-    // res.render('public/index.html');
     res.sendFile(path.resolve(__dirname,'public/index.html'));
 });
 
 app.get('/about',(req,res) => {
-    //res.render('public/about.html');
     res.sendFile(path.resolve(__dirname,'public/about.html'));
 });
-/*app.get('/about',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'public/pages/about.html'))
-});*/
-
-/*app.get('/contact',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'public/pages/contact.html'))
-});
-
-app.get('/post',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'public/pages/post.html'))
-});*/
