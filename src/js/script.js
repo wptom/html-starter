81d1adef0d249aaef7c0970bf0bbfecce3da8d9f
wptom.com <hello@wptom.com>
2022-02-14 20:50:58 +0100

    Stylelint settings + menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-console.log('test');
+// ***********************************************************
+// Navigation
+// ***********************************************************
+let headerNavigation = document.querySelector('nav');
+
+document.querySelector('.nav-hamburger')
+    .addEventListener('click', function() {
+        console.log('clicked');
+        headerNavigation.classList.toggle('nav--visible');
+    });
