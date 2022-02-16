883c8cfb64a8dd89420b6d28311247f5091ec6e5
wptom.com <hello@wptom.com>
2022-02-16 17:03:26 +0100

    Path to bootstrap node-sass replaced with sass

--- src/js/script.js
+++ src/js/script.js
@@ -6,5 +6,5 @@ let headerNavigation = document.querySelector('nav');
 document.querySelector('.nav-hamburger')
     .addEventListener('click', function() {
         console.log('clicked');
-        headerNavigation.classList.toggle('nav--visible');
+        headerNavigation.classList.toggle('header-nav--visible');
     });
