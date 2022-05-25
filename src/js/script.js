a2ff7ac90fe3678b5652afef70369d2ac8a1ef8a
wptom.com <tom@wptom.com>
2022-05-25 14:35:43 +0200

    Sections styling.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,21 @@
 // ***********************************************************
+// Testimonials
+// ***********************************************************
+let testimonialsSlider = tns({
+    container: '.testimonials-slider',
+    controls: true,
+    controlsPosition: 'bottom',
+    controlsContainer: '#customize-controls',
+    nav: false,
+    navAsThumbnails: false,
+    items: 1,
+    slideBy: 'page',
+    center: true,
+    mouseDrag: true,
+    autoplay: false
+});
+
+// ***********************************************************
 // Navigation
 // ***********************************************************
 let headerNavigation = document.querySelector('nav');
@@ -8,3 +25,21 @@ document.querySelector('.nav-hamburger')
         console.log('clicked');
         headerNavigation.classList.toggle('header-nav--visible');
     });
+
+// ***********************************************************
+// Accordion
+// ***********************************************************
+
+let acc = document.getElementsByClassName('js-accordion-title');
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function() {
+        this.classList.toggle('accordion__titlea--active');
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
+}
