524501cf686ec671cd33934b2a12cae63100e5fe
wptom.com <tom@wptom.com>
2022-06-01 20:38:58 +0200

    Menu javascript

--- src/js/modules/header-nav.js
+++ src/js/modules/header-nav.js
@@ -8,9 +8,19 @@ class HeaderNav {
 
         document.querySelector('.nav-hamburger')
             .addEventListener('click', function() {
-                console.log('clicked');
                 headerNavigation.classList.toggle('header-nav--visible');
             });
+
+        document.querySelectorAll('.header-nav__list__item__link').forEach(item => {
+            item.addEventListener('click', function() {
+                headerNavigation.classList.remove('header-nav--visible');
+
+                document.querySelectorAll('.header-nav__list__item__link').forEach(item => {
+                    item.parentNode.classList.remove('header-nav__list__item--active');
+                });
+                item.parentNode.classList.add('header-nav__list__item--active');
+            });
+        })
     }
 }
 
